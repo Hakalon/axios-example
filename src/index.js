59ed62499cccdb9212de91e056a08e2fa1db0e56
Steve Lai <tf4ewg@gmail.com>
2025-06-04 21:49:50 +0800

    Fix config and documentation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 const axios = require('axios');
 const JsSHA = require('jssha');
 
+function getAuthorizationHeader() {
+  const appID = '31c5632cc08d4421abf734566fd50ed2';
+  const appKey = 'CNaXup9EDeew3oyoa8N8CWdE2ZI';
+  const GMTString = new Date().toUTCString();
+  const shaObj = new JsSHA('SHA-1', 'TEXT');
+
+  shaObj.setHMACKey(appKey, 'TEXT');
+  shaObj.update(`x-date: ${GMTString}`);
+
+  const HMAC = shaObj.getHMAC('B64');
+  const authorization = `hmac username="${appID}", algorithm="hmac-sha1", headers="x-date", signature="${HMAC}"`;
+
+  // 若要減少傳輸可以再加上Accept-Encoding: 'gzip'，但使用時就必須額外做解壓縮的動作，否則會得到一串亂碼。
+  return {
+    Authorization: authorization, 'X-Date': GMTString
+  };
+}
+
 const headers = getAuthorizationHeader();
 const city = 'Taipei';
 const route = '912';
 
 const motcReqSender = axios.create({
-  baseURL: 'http://ptx.transportdata.tw/MOTC/v2/Bus/RealTimeByFrequency/City/',
+  baseURL: 'https://ptx.transportdata.tw/MOTC/v2/Bus/RealTimeByFrequency/City/',
   headers
 });
 
@@ -21,22 +39,3 @@ motcReqSender.get(`${city}/${route}?$top=1&$format=JSON`)
     console.log('There is something wrong: ');
     console.log(err);
   });
-
-// MOTC授權認證
-function getAuthorizationHeader() {
-  const appID = '31c5632cc08d4421abf734566fd50ed2';
-  const appKey = 'CNaXup9EDeew3oyoa8N8CWdE2ZI';
-  const GMTString = new Date().toGMTString();
-  const shaObj = new JsSHA('SHA-1', 'TEXT');
-
-  shaObj.setHMACKey(appKey, 'TEXT');
-  shaObj.update(`x-date: ${GMTString}`);
-
-  const HMAC = shaObj.getHMAC('B64');
-  const authorization = `hmac username="${appID}", algorithm="hmac-sha1", headers="x-date", signature="${HMAC}"`;
-
-  // 若要減少傳輸可以再加上Accept-Encoding: 'gzip'，但使用時就必須額外做解壓縮的動作，否則會得到一串亂碼。
-  return {
-    Authorization: authorization, 'X-Date': GMTString
-  };
-}
